8932974d47a0db34ea5194b918dac5393e097b7a
Chenghao Sun <601330984@qq.com>
2019-07-12 19:15:49 +0800

    准备发布

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   let ENV = {
-    modulePrefix: 'bmweb',
+    modulePrefix: 'bm-off-page',
     environment,
     rootURL: '/',
     locationType: 'auto',
